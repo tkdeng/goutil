d7cb991dd14989244de8792927dbab2f9776ba3e
tkdeng <web@tkdeng.com>
2024-09-11 14:12:30 -0400

    added methods

--- goutil.go
+++ goutil.go
@@ -3,8 +3,9 @@ package goutil
 import (
 	"errors"
 	"strconv"
+	"strings"
 
-	"github.com/tkdeng/goregex"
+	regex "github.com/tkdeng/goregex"
 )
 
 // Contains returns true if an array contains a value
@@ -116,4 +117,131 @@ func Swap[T any](val1 *T, val2 *T) {
 	v := *val1
 	*val1 = *val2
 	*val2 = v
+}
+
+// ToColumns sorts a list of strings into rows and columns
+//
+// The column size will be wrapped by words first, and wrap to the specisied size.
+//
+// This method is useful for printing help info to a terminal.
+//
+// example:
+//
+//	stty := 80 // import "golang.org/x/term"
+//	if width, _, err := term.GetSize(0); err == nil {
+//	  stty = width
+//	}
+//
+//	goutil.ToColumns([][]string{
+//	  {"--opt1", "info"}
+//	  {"--opt2, -o", "more info"}
+//	}, stty, "    ", "\n\n")
+//
+// @sep (optional): column separator || row separator
+//   - if "\n" is included: this will assume a row separator.
+//   - else: this will assume a column separator
+func ToColumns(list [][]string, charSize int, sep ...string) string {
+	maxColSize := []int{}
+
+	colSep := "    "
+	rowSep := "\n\n"
+
+	for _, s := range sep {
+		if strings.ContainsRune(s, '\n') {
+			rowSep = s
+		} else {
+			colSep = s
+		}
+	}
+
+	for r, col := range list {
+		for len(maxColSize) < len(col) {
+			maxColSize = append(maxColSize, 0)
+		}
+
+		for len(list[r]) < len(maxColSize) {
+			list[r] = append(list[r], "")
+		}
+
+		for i, val := range col {
+			if len(val) > maxColSize[i] {
+				maxColSize[i] = len(val)
+			}
+		}
+	}
+
+	maxSize := 0
+	for i, s := range maxColSize {
+		if i == len(maxColSize)-1 {
+			if s := maxSize + len(colSep) + 1; s > charSize {
+				charSize = s
+			}
+		} else {
+			maxColSize[i] += len(colSep)
+		}
+
+		maxSize += s
+	}
+
+	for r, col := range list {
+		colSize := 0
+
+		for i, val := range col {
+			size := maxColSize[i]
+			if i != len(maxColSize)-1 {
+				size -= len(colSep)
+
+				if len(val) < size {
+					list[r][i] += strings.Repeat(" ", size-len(val))
+				}
+
+				list[r][i] += colSep
+
+				colSize += len(list[r][i])
+			} else if size := charSize - colSize; len(val) > size {
+				p := 0
+
+				for len(val) > size+p {
+					s := size + p
+
+					hasU := false
+					for u := s; u > p; u-- {
+						if val[u] == ' ' {
+							s = u
+							hasU = true
+							break
+						}
+					}
+
+					if hasU {
+						val = val[:s] + "\n" + strings.Repeat(" ", colSize) + val[s+1:]
+						p += colSize
+					} else {
+						val = val[:s] + "\n" + strings.Repeat(" ", colSize) + val[s:]
+						p += colSize + 1
+					}
+					p += s
+				}
+
+				// fix long words
+				valList := strings.Split(val, "\n")
+
+				for v := 0; v < len(valList); v++ {
+					if len(valList[v]) > charSize {
+						valList = append(valList, strings.Repeat(" ", colSize)+valList[v][charSize:])
+						valList[v] = valList[v][:charSize]
+					}
+				}
+
+				list[r][i] = strings.Join(valList, "\n")
+			}
+		}
+	}
+
+	res := ""
+	for _, col := range list {
+		res += strings.Join(col, "") + rowSep
+	}
+
+	return res
 }
